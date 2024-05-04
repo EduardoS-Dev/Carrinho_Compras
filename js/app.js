71449eb5efc3b20a470debde0d0b03f4f78a545c
Eduardo Soares <eduardo.soaressilva@icloud.com>
2024-05-04 18:48:50 -0300

    Solicitando quantidade e confirmacao para limpar carrinho

--- js/app.js
+++ js/app.js
@@ -7,26 +7,34 @@ function adicionar(){
     let produtoSelecionado = document.getElementById('produto').value;
     let quantidade = document.getElementById('quantidade').value;
 
-    //Guardando o campo do produto no carrinho para modificar assim que for selecionado o produto/quantidade
-    let carrinhoCompras = document.getElementById('lista-produtos');
+    if(quantidade <= 0){
+        alert('Informe a quantidade');
+    } else {
+        //Guardando o campo do produto no carrinho para modificar assim que for selecionado o produto/quantidade
+        let carrinhoCompras = document.getElementById('lista-produtos');
+            
+
+        //separando os valores do produto selecionado  "Fone de ouvido - R$100" 
+        let nomeProduto = produtoSelecionado.split('-')[0];
+        let valorProduto = produtoSelecionado.split('R$')[1];
+
+        //Valor do item x quantidade
+        let preco = quantidade * valorProduto;
+        carrinhoCompras.innerHTML += `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
+        </section>`
+
+        totalGeral = totalGeral + preco;
+        let campoTotal = document.getElementById('valor-total');
+        campoTotal.textContent = `R$ ${totalGeral}`;
+    }
     
-
-    //separando os valores do produto selecionado  "Fone de ouvido - R$100" 
-    let nomeProduto = produtoSelecionado.split('-')[0];
-    let valorProduto = produtoSelecionado.split('R$')[1];
-
-    //Valor do item x quantidade
-    let preco = quantidade * valorProduto;
-    carrinhoCompras.innerHTML += `<section class="carrinho__produtos__produto">
-    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
-  </section>`
-
-    totalGeral = totalGeral + preco;
-    let campoTotal = document.getElementById('valor-total');
-    campoTotal.textContent = `R$ ${totalGeral}`;
 }
 
 function limpar(){
-    document.getElementById('lista-produtos').innerHTML = '';
-    document.getElementById('valor-total').textContent = 'R$ 0';
+    if(confirm('Deseja limpar o carrinho? ')){
+        document.getElementById('lista-produtos').innerHTML = '';
+        document.getElementById('valor-total').textContent = 'R$ 0';
+    }
+    
 }
